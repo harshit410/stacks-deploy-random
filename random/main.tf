be9255ca808357249c3c4ae4aa87865a5f6957a3
Harshit Gupta <harshit.gupta@hashicorp.com>
2025-11-11 16:40:11 +0530

    adding more resoruces

--- random/main.tf
+++ random/main.tf
@@ -1,6 +1,7 @@
 resource "random_pet" "increase_resource_count" {
-  for_each = { for idx in range(1, 10) : idx => idx }
+  for_each = { for idx in range(1, 15) : idx => idx }
 
   prefix = "hello-world-${each.key}"
   length = 4
 }
+ 
